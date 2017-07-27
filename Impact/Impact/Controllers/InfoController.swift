98d5e4a5a82bc2e7736e87bdcbbc15d59529f738
concon55 <clg469@nyu.edu>
2017-07-27 09:56:07 -0400

    Second commit.

---
+++ Impact/Impact/Controllers/InfoController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoController.swift
+//  Impact
+//
+//  Created by Connie Guan on 7/27/17.
+//  Copyright © 2017 Connie Guan. All rights reserved.
+//
+
+import UIKit
+
+class InfoController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
